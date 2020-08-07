e5df6dc90ca0c0a5b8d425dbfc5fe83d4dda525d
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-08-07 19:03:50 +0100

    Adding service account

--- func/coucou.js
+++ func/coucou.js
@@ -1,5 +1,5 @@
 exports.handler = async (event, context) => {
-    console.log(event);
+    console.log(SERVICE_ACCOUNT);
     return {
         statusCode: 200,
         body: "<h1>Tout se passe bien!</h1>"
---
+++ utils.js
@@ -0,0 +1,46 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./badol-65208-firebase-adminsdk-iztyi-47aad9a9a2.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://badol-65208.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+exports.s = async function (collection, params) {
+
+    const champ = Object.keys(params)[0];
+    const valeur = params[champ];
+
+    const ref = db.collection(collection);
+    const snapshot = await ref.where(champ, '==', valeur).get();
+    if (snapshot.empty) {
+        console.log('No matching documents.');
+        return null;
+    } else {
+        return snapshot.docs;
+    }
+}
+
+exports.one = async function (collection, params) {
+
+    const champ = Object.keys(params)[0];
+    const valeur = params[champ];
+
+    const ref = db.collection(collection);
+    const snapshot = await ref.where(champ, '==', valeur).get();
+    if (snapshot.empty) {
+        return null;
+    } else {
+        return snapshot.docs[0];
+    }
+}
+
+exports.e = async function (collection, doc) {
+
+    const res = await db.collection(collection).add(doc);
+
+    return res.id;
+}
