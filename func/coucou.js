cc19dbcf38f1a882426ce60879d86646b7ee4ea7
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-08-07 19:19:20 +0100

    Edit environment variable

--- func/coucou.js
+++ func/coucou.js
@@ -1,5 +1,5 @@
 exports.handler = async (event, context) => {
-    console.log(SERVICE_ACCOUNT);
+    console.log(TEST_NAME);
     return {
         statusCode: 200,
         body: "<h1>Tout se passe bien!</h1>"
