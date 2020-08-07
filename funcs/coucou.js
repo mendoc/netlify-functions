c4de8098e264d7d04038e32fccaf0c676311272f
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-08-07 20:40:42 +0100

    Config service account

--- funcs/coucou.js
+++ funcs/coucou.js
@@ -1,7 +1,6 @@
 exports.handler = async (event, context) => {
-    console.log(process.env);
     return {
         statusCode: 200,
-        body: "<h1>Tout se passe bien!</h1>"
+        body: process.env.SERVICE_ACCOUNT
     }
 }
