4fe51346dde30e3c344e7f3f78e60741836145c8
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-08-08 05:32:15 +0100

    Adding firebase admin

--- funcs/coucou.js
+++ funcs/coucou.js
@@ -1,11 +1,31 @@
-const { one, e } = require('./utils');
+const admin = require('firebase-admin');
 
-console.log(one);
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.SERVICE_ACCOUNT
+});
+
+const db = admin.firestore();
+
+const s = async (collection) => {
+
+    const snapshot = await db.collection(collection).get();
+    if (snapshot.empty) {
+        console.log('No documents found.');
+        return [];
+    } else {
+        return snapshot.docs;
+    }
+}
 
 exports.handler = async (event, context) => {
+    let comptes = s("comptes");
+    
     return {
         statusCode: 200,
-        headers: { "Content-Type": "application/json"},
-        body: process.env.SERVICE_ACCOUNT
+        headers: { "Content-Type": "application/json" },
+        body: comptes
     }
 }
