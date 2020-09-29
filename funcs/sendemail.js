5c95e32058a723147ffde3324298d73ca153c7eb
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-09-29 19:57:16 +0100

    Génération du UUID

--- funcs/sendemail.js
+++ funcs/sendemail.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-var nodemailer = require('nodemailer');
+const { v4: uuidv4 } = require('uuid');
+var nodemailer       = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -10,15 +11,26 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+const token = uuidv4();
+const URL = `https://gombo.ga/#/password?t=${token}`
+
 var mailOptions = {
     from: `Gombo <${process.env.EMAIL_USER}>`,
     to: 'ongouadimitri5@gmail.com',
     subject: 'Changement de mot de passe',
-    text: 'Vous recevez ce message car vous avez signalé avoir oublié votre mot de passe. Suivez le lien ci-dessous pour définir un nouveau mot de passe. https://gombo.ga/#/password',
-    html: 'Vous recevez ce message car vous avez signalé avoir oublié votre mot de passe. <br><br>Suivez le lien ci-dessous pour définir un nouveau mot de passe. https://gombo.ga/#/password'
+    text: `Vous recevez ce message car vous avez signalé avoir oublié votre mot de passe. Suivez le lien ci-dessous pour définir un nouveau mot de passe. ${URL}`,
+    html: `Vous recevez ce message car vous avez signalé avoir oublié votre mot de passe. <br><br>Suivez le lien ci-dessous pour définir un nouveau mot de passe. ${URL}`
 };
 
 exports.handler = (event, context, callback) => {
+    if (event.httpMethod !== "POST") {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({message: "Method Not Allowed", url: URL})
+        });
+        return;
+    }
+
     transporter.sendMail(mailOptions, function (error, info) {
         let message = "";
         if (error) {
