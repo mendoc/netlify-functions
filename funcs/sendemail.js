2305e84485ba716484f43d69e360ed8d5da1588b
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-09-29 15:21:58 +0100

    Test d'envoi de mail

---
+++ funcs/sendemail.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+var mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'ongouadimitri5@gmail.com',
+    subject: 'Changement de mot de passe',
+    text: 'Vous recevez ce message car vous avez signalé avoir oublié votre mot de passe. Suivez le lien ci-dessous pour définir un nouveau mot de passe. https://gombo.ga/#/password'
+};
+
+exports.handler = async (event, context, callback) => {
+    return transporter.sendMail(mailOptions, function (error, info) {
+        let message = "";
+        if (error) {
+            message = error.response;
+        } else {
+            message = 'Email envoyé: ' + info.response;
+        }
+        callback(null, {
+            statusCode: 200,
+            body: message
+        });
+    });
+
+}
