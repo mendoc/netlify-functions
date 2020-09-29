87bd4969b6f2ff2445e8835cabc7daaddd0bd0c6
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2020-09-29 15:55:54 +0100

    Ajout de corps HTML

--- funcs/sendemail.js
+++ funcs/sendemail.js
@@ -14,7 +14,8 @@ var mailOptions = {
     from: `Gombo <${process.env.EMAIL_USER}>`,
     to: 'ongouadimitri5@gmail.com',
     subject: 'Changement de mot de passe',
-    text: 'Vous recevez ce message car vous avez signalé avoir oublié votre mot de passe. <br><br>Suivez le lien ci-dessous pour définir un nouveau mot de passe. https://gombo.ga/#/password'
+    text: 'Vous recevez ce message car vous avez signalé avoir oublié votre mot de passe. Suivez le lien ci-dessous pour définir un nouveau mot de passe. https://gombo.ga/#/password',
+    html: 'Vous recevez ce message car vous avez signalé avoir oublié votre mot de passe. <br><br>Suivez le lien ci-dessous pour définir un nouveau mot de passe. https://gombo.ga/#/password'
 };
 
 exports.handler = (event, context, callback) => {
